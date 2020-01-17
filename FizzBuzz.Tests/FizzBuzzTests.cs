a14d553bdda19751972a023e596e6958af151ffc
rhi-teetzel <rhiannateetzel@gmail.com>
2020-01-16 22:02:53 -0500

    First commit; without collaboration at home.

---
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,40 @@
+using System;
+using Xunit;
+
+namespace FizzBuzz.Tests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void Get_Returns_1_for_1()
+        {
+            var fizzBuzz = new FizzBuzz();
+
+            var result = fizzBuzz.Get(1);
+
+            Assert.Equal("1", result);
+        }  
+        
+        [Fact]
+        public void Get_Returns_2_for_2()
+        {
+            var fizzBuzz = new FizzBuzz();
+
+            var result = fizzBuzz.Get(2);
+
+            Assert.Equal("2", result);
+        }
+
+        [Fact]
+
+        public void Get_Returns_Fizz_for_3()
+        {
+            var fizzBuzz = new FizzBuzz();
+
+            var result = fizzBuzz.Get(6);
+
+            Assert.Equal("Fizz", result);
+
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string Get(int num)
+        {
+            if (num == 3)
+            {
+                return "Fizz";
+            }
+            return num.ToString();
+        }
+    }
+}
